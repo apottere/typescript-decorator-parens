1aa7a1e839f94abdb71129dd11087cbff145e5a5
Andrew Potter <apottere@gmail.com>
2023-11-09 09:34:03 -0500

    Initial commit

---
+++ src/no-parens.ts
@@ -0,0 +1,10 @@
+function GenericDecorator<_T>(_target: any, _key: string, descriptor: PropertyDescriptor) {
+    return descriptor;
+}
+
+export class SomeClass {
+    @GenericDecorator<'test'>
+    public someFunction() {
+        return 42;
+    }
+}
